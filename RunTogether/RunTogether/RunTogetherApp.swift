688e9cad7f2ea18825fe8b10900fbb881d4fafba
Utkarsh Mishra <112176434+utkarshmishra1@users.noreply.github.com>
2026-04-19 12:42:19 +0530

    Initial Xcode project

---
+++ RunTogether/RunTogether/RunTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTogetherApp.swift
+//  RunTogether
+//
+//  Created by utkarsh mishra on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct RunTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
