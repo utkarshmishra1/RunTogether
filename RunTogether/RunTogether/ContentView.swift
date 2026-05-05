9f74e19026ea7315b89fdb472100887cf8937d7e
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2026-05-05 18:31:08 +0530

    push

--- RunTogether/RunTogether/ContentView.swift
+++ RunTogether/RunTogether/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!fjkgnlkrjng")
+            Text("Hello, Runner")
         }
         .padding()
     }
